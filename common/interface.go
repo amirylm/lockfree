e9e32d13d3af4dc963bff323d9fddab7f7b2d545
amirylm <amirylm.dev@gmail.com>
2023-06-25 18:14:04 +0300

    comments

--- common/interface.go
+++ common/interface.go
@@ -10,8 +10,10 @@ var (
 	ErrOverflow = errors.New("data overflow")
 )
 
+// DataStructureFactory is a factory function for creating instances of ds
 type DataStructureFactory[Value any] func(capacity int) DataStructure[Value]
 
+// DataStructure is implemented by all the data structures in this project.
 type DataStructure[Value any] interface {
 	Push(Value) bool
 	Pop() (Value, bool)
@@ -20,6 +22,9 @@ type DataStructure[Value any] interface {
 	Full() bool
 }
 
+// Push is used as a safe function to write values into the ds,
+// ensuring that even if the q is full, we'll try to write the item
+// until the context is done.
 func Push[Value any](ctx context.Context, ds DataStructure[Value], val Value) bool {
 	for !ds.Push(val) {
 		if ctx.Err() != nil {
@@ -30,6 +35,8 @@ func Push[Value any](ctx context.Context, ds DataStructure[Value], val Value) bo
 	return true
 }
 
+// Pop is a safe function to read values from the ds,
+// until the context is done.
 func Pop[Value any](ctx context.Context, ds DataStructure[Value]) (Value, bool) {
 	element, ok := ds.Pop()
 	for !ok && ctx.Err() == nil {
